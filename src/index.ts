d6e230610e218e423cd1d4f5a480da2114670da1
Patricia <patricia.femenias@gmail.com>
2023-11-13 18:34:17 +0100

    Add app.ts and dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,4 @@
-import express from "express";
+import app from "./app.js";
 import morgan from "morgan";
 
-const app = express();
-
-const port = process.env.PORT ?? 4000;
-
-app.listen(+port, () => {
-  console.log("Estoy escuchando...");
-});
-
 app.use(morgan("dev"));
-
-export default app;
