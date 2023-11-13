cafd919d903d0894be011952d2e8e7add318f0b2
Patricia <patricia.femenias@gmail.com>
2023-11-13 20:31:24 +0100

    Add tools data

---
+++ src/features/tools/data/tools.ts
@@ -0,0 +1,34 @@
+import { Tool } from "../../../types";
+
+const tools: Tool[] = [
+  {
+    id: 1,
+    ToolName: "Javascript",
+    difficultyLevel: 9,
+  },
+  {
+    id: 2,
+    ToolName: "Css",
+    difficultyLevel: 6,
+  },
+  {
+    id: 3,
+    ToolName: "HTML",
+    difficultyLevel: 5,
+  },
+  {
+    id: 4,
+    ToolName: "Css",
+    difficultyLevel: 6,
+  },
+  {
+    id: 5,
+    ToolName: "Git",
+    difficultyLevel: 6,
+  },
+  {
+    id: 6,
+    ToolName: "Reat",
+    difficultyLevel: 8,
+  },
+];
