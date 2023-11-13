e0f5ba4df6b6460a7dd2387bdee46c980b9ca06d
Patricia <patricia.femenias@gmail.com>
2023-11-13 23:36:44 +0100

    Add DELETE method

--- src/features/tools/router/toolsRouter.ts
+++ src/features/tools/router/toolsRouter.ts
@@ -20,4 +20,18 @@ toolsRouter.get("/:idTool", (req, res) => {
   res.status(200).json(tool);
 });
 
+toolsRouter.delete("/:idTool", (req, res) => {
+  const id = Number(req.params.idTool);
+
+  const toolIndex = tools.findIndex((tool) => tool.id === id);
+
+  if (toolIndex === -1) {
+    res.status(404).json({ error: "This tool doesn't exist" });
+  }
+
+  tools.splice(toolIndex, 1);
+
+  res.status(200).json({});
+});
+
 export default toolsRouter;
