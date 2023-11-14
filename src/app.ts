879a1297f49edf1d558513a5af368c4d7f69cfd3
Patricia <patricia.femenias@gmail.com>
2023-11-14 09:40:27 +0100

    Add missing Morgan typed

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import "dotenv/config";
 import express from "express";
+import chalk from "chalk";
 
 const app = express();
 
 const port = process.env.PORT ?? 4000;
 
 app.listen(+port, () => {
-  console.log(`Listening on http://localhost:${port}`);
+  console.log(chalk.green`Listening on http://localhost:${port}`);
 });
 
 export default app;
